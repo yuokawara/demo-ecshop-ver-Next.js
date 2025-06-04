cc3192429506046a486fd27bf5ed2e2b2933e4e4
yuokawara <solbonta@hotmail.com>
2025-06-04 12:03:49 +0900

    micro cms検証 builderror検証5

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ['images.ctfassets.net', 'images.microcms-assets.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
